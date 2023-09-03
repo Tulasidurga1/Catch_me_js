2ddd1622f1ff3e942d156006a7a05e87f97ff630
Tulasidurga1 <jonnatulasidurga111@gmail.com>
2023-09-03 21:12:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const button=document.getElementById("box")
+
+button.addEventListener("mouseover",()=>{
+    const top=Math.floor(Math.random()*500)
+    const left=Math.floor(Math.random()*1000)
+   
+    button.style.left=left+"px"
+    button.style.top=top+"px"
+    
+})
+
+
+
+
